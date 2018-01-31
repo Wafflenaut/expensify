7539cd6c116a1b44e593f0be1bf13adb841c69d8
jmoyer <moyerjo@oregonstate.edu>
2018-01-31 14:04:27 -0500

    Further integration of react/redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouters';
 import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
@@ -12,10 +13,20 @@ const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water Bill'}));
 store.dispatch(addExpense({ description: 'Gas Bill' }));
-store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+	store.dispatch(setTextFilter('rent'));
+}, 3000);
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+	<Provider store={store}>
+		<AppRouter />
+	</Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));

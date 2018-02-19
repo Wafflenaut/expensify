fa16c59cc232dbc218e34d39b66c6be48e5e3341
Wafflenaut <moyerjo@oregonstate.edu>
2018-02-19 17:37:41 -0500

    modified webpack config for styles

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -7,7 +7,7 @@ export const ExpenseList = (props) => (
 	<div>
 		{
 			props.expenses.length === 0 ? (
-				<p>No epenses</p>
+				<p>No expenses</p>
 			) : (
 				props.expenses.map((expense) => (
 					<ExpenseListItem key={expense.id} {...expense} />
